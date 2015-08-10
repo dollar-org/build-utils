e66ce4be66684bb24701850d38ebfe7728d62682
neilellis <hello@neilellis.me>
2015-08-10 21:19:21 +0100

    Fixing envsubst bug

--- promote_from_staging.sh
+++ promote_from_staging.sh
@@ -15,7 +15,10 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git reset HEAD --hard
 git clean -fd
-envsubst '${RELEASE}' < README.md > /tmp/README.expanded
+
+BLURB='**If you use this library please consider giving us a star on GitHub, also if you can spare 30 secs of your time please let us know your priorities here: https://sillelien.wufoo.com/forms/zv51vc704q9ary/  - thanks**'
+
+envsubst '${RELEASE}:${BLURB}' < README.md > /tmp/README.expanded
 git checkout master
 git pull 
 git merge staging -m "Auto merge"
