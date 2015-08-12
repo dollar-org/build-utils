dd51ddd99c81792d8472c072f59ed71f996245a6
neilellis <hello@neilellis.me>
2015-08-12 15:51:57 +0100

    State badges

--- promote_from_staging.sh
+++ promote_from_staging.sh
@@ -36,7 +36,7 @@ cat <<EOF
 
 Please contact us through chat or through GitHub Issues.
 
-[![GitHub Issues](https://img.shields.io/github/issues/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.svg)](https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues).
+[![GitHub Issues](https://img.shields.io/github/issues/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.svg)](https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues)
 
 [![Join the chat at https://gitter.im/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
