634ce99ecbd418f9b6a85346c61fc15b02a52727
Neil Ellis <hello@neilellis.me>
2015-09-04 19:29:56 +0100

    release tags

--- promote_from_staging.sh
+++ promote_from_staging.sh
@@ -92,9 +92,6 @@ export HEADER=""
 export TUTUM="[![Deploy to Tutum](https://s.tutum.co/deploy-to-tutum.svg)](https://dashboard.tutum.co/stack/deploy/)"
 
 git pull origin
-git checkout master
-git pull origin
-git merge ${CIRCLE_BRANCH} -m "Auto merge"
 echo ${RELEASE} > .release
 echo ${RELEASE} ${TAG} ${CODENAME} ${CIRCLE_SHA1} > .release.details
 envsubst '${RELEASE}:${BLURB}:${FOOTER}:${HEADER}:${STATE_SHELVED}:${STATE_EXPERIMENTAL}:${STATE_ACTIVE}:${STATE_PRE_ALPHA}:${STATE_ALPHA}:${STATE_BETA}:${STATE_PROD}:${TUTUM}' < README.tmpl.md > README.md
@@ -102,10 +99,13 @@ if [ -f tutum.tmpl.yml ]
 then
     envsubst '${RELEASE}' < tutum.tmpl.yml > tutum.yml
 fi
-git add README.md .release .release.details
-git commit -a -m "Promotion of ${RELEASE}" || :
-git fetch
+git add README.md .release .release.details tutum.yml
+git commit -a -m "Templates for ${RELEASE}" || :
+git push
+git checkout master
+git pull origin
 git merge ${CIRCLE_BRANCH} -m "Auto merge"
+git commit -a -m "Promotion of ${RELEASE}" || :
 git push
 git tag ${TAG} || :
 git push --tags
