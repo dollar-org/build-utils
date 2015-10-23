39df596b65ae1fcc56827750429699848229b75f
neilellis <hello@neilellis.me>
2015-10-23 18:50:42 +0100

    build functions

--- functions.sh
+++ functions.sh
@@ -144,16 +144,6 @@ function routes3() {
                 "Protocol": "https",
                 "ReplaceKeyPrefixWith": "$live/"
             }
-        }, {
-            "Condition": {
-                "HttpErrorCodeReturnedEquals": "404"
-            },
-            "Redirect": {
-                "HostName": "$1",
-                "HttpRedirectCode": "302",
-                "Protocol": "https",
-                "ReplaceKeyWith": "live/"
-            }
         }
     ]
 }
