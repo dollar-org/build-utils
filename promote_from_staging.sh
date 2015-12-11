50bb6e421a89c69f75ec5c4ad1fe28f98085f436
neilellis <hello@neilellis.me>
2015-12-11 21:29:49 +0000

    Build Utilities

--- promote_from_staging.sh
+++ promote_from_staging.sh
@@ -95,12 +95,12 @@ export TUTUM="[![Deploy to Tutum](https://s.tutum.co/deploy-to-tutum.svg)](https
 
 git notes --ref=version add -m "${RELEASE}" ${CIRCLE_SHA1}
 git notes --ref=codename add -m "${CODENAME}" ${CIRCLE_SHA1}
+git branch --set-upstream-to=origin/${CIRCLE_BRANCH} ${CIRCLE_BRANCH}
 git push origin "refs/notes/*"
 
 git checkout -f master
 git pull -f -n <<< "Rebasing master"
 git config --global push.default simple
-git branch --set-upstream-to=origin/${CIRCLE_BRANCH} ${CIRCLE_BRANCH}
 git checkout ${CIRCLE_BRANCH}
 git rebase master
 git checkout master
