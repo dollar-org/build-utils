0c5205567f9a4cc3d1a2589893eac06c1963b51c
neilellis <hello@neilellis.me>
2015-12-12 00:59:46 +0000

    Build Utilities

--- functions.sh
+++ functions.sh
@@ -15,6 +15,8 @@ fi
 export TAG=${RELEASE}
 export release=${RELEASE}
 export AWS_DEFAULT_REGION=eu-west-1
+export environment=${CIRCLE_BRANCH:-local}
+
 
 
 changed() {
@@ -109,6 +111,25 @@ dflatten() {
     docker kill $cont
 }
 
+s3_deploy() {
+
+    if [[ $environment == master ]]
+    then
+        export DEPLOY_PREFIX=${CODENAME}
+        envsubst < ${build_util_dir}redirect.html > out/redirect-expanded.html
+        cp -f  ${build_util_dir}/s3_website.yml .
+        s3_website cfg apply --headless
+        s3_website push
+        #   Wait for everything to be ready
+        sleep 60
+        aws s3 cp --quiet --cache-control "max-age=10" out/redirect-expanded.html s3://${DEPLOY_HOST}/index.html
+    else
+        export DEPLOY_PREFIX=${environment}
+        aws s3 sync  --delete --cache-control "max-age=0, no-cache, no-store, private" --expires "" --exclude "*.ts"  --exclude "*assets/scss/*" --exclude "*typings/*"   out/ s3://${DEPLOY_HOST}/${environment}/
+    fi
+
+}
+
 
 #if [[ -n ${DOCKER_USER} ]] && which docker
 #then
@@ -123,50 +144,50 @@ dflatten() {
 
 
 
-function routes3() {
-    aws s3 sync --quiet --delete --cache-control "no-cache" s3://$1/${3}/ s3://$1/previous/
-    echo ${4} > /tmp/version
-    echo ${4},${codename:-},${CIRCLE_BUILD_NUM},${CIRCLE_SHA1},${CIRCLE_COMPARE_URL},$(date) > /tmp/build
-    aws s3 cp  /tmp/version s3://$1/${4}/.version
-    aws s3 cp /tmp/build s3://$1/${4}/.build
-    aws s3 cp /tmp/version s3://$1/${3}/.stale
-
-    if [[ $2 == "forward" ]]
-    then
-        live=${4}
-    else
-        live=${3}
-    fi
-    aws s3 sync --quiet --delete --cache-control "no-cache" s3://$1/${live}/ s3://$1/current/
-
-    ws_config=$(
-    cat << EOF
-    {
-        "ErrorDocument": {
-            "Key": "error.html"
-        },
-        "IndexDocument": {
-            "Suffix": "index.html"
-        },
-        "RoutingRules": [
-            {
-                "Condition": {
-                    "KeyPrefixEquals": "live/"
-                },
-                "Redirect": {
-                    "HostName": "$1",
-                "HttpRedirectCode": "302",
-                "Protocol": "https",
-                "ReplaceKeyPrefixWith": "$live/"
-            }
-        }
-    ]
-}
-
-EOF
-)
-
-    aws s3api put-bucket-website --bucket $1 --website-configuration "$ws_config"
-
-}
-
+#function routes3() {
+#    aws s3 sync --quiet --delete --cache-control "no-cache" s3://$1/${3}/ s3://$1/previous/
+#    echo ${4} > /tmp/version
+#    echo ${4},${codename:-},${CIRCLE_BUILD_NUM},${CIRCLE_SHA1},${CIRCLE_COMPARE_URL},$(date) > /tmp/build
+#    aws s3 cp  /tmp/version s3://$1/${4}/.version
+#    aws s3 cp /tmp/build s3://$1/${4}/.build
+#    aws s3 cp /tmp/version s3://$1/${3}/.stale
+#
+#    if [[ $2 == "forward" ]]
+#    then
+#        live=${4}
+#    else
+#        live=${3}
+#    fi
+#    aws s3 sync --quiet --delete --cache-control "no-cache" s3://$1/${live}/ s3://$1/current/
+#
+#    ws_config=$(
+#    cat << EOF
+#    {
+#        "ErrorDocument": {
+#            "Key": "error.html"
+#        },
+#        "IndexDocument": {
+#            "Suffix": "index.html"
+#        },
+#        "RoutingRules": [
+#            {
+#                "Condition": {
+#                    "KeyPrefixEquals": "live/"
+#                },
+#                "Redirect": {
+#                    "HostName": "$1",
+#                "HttpRedirectCode": "302",
+#                "Protocol": "https",
+#                "ReplaceKeyPrefixWith": "$live/"
+#            }
+#        }
+#    ]
+#}
+#
+#EOF
+#)
+#
+#    aws s3api put-bucket-website --bucket $1 --website-configuration "$ws_config"
+#
+#}
+#
