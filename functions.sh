53417e8c0196ebf20f6eae1c7391cfe0d69bc974
neilellis <hello@neilellis.me>
2015-12-15 23:14:06 +0000

    Build Utilities

--- functions.sh
+++ functions.sh
@@ -7,7 +7,7 @@ if [[ -n $CI ]]
 then
     export CODENAME=$($build_util_dir/codenames/name.sh $CIRCLE_SHA1)
     export RELEASE=${RELEASE:-${CODENAME}-${CIRCLE_BUILD_NUM}}
-    export NUMERIC_RELEASE=${$CIRCLE_BUILD_NUM}
+    export NUMERIC_RELEASE=${CIRCLE_BUILD_NUM}
 else
     export RELEASE=local
     export CODENAME=local
