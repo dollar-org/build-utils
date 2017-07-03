1203e0a4128fd2ee3df65895808d7453b07a67c0
neilellis <neil@vizz.buzz>
2017-07-03 17:24:59 +0100

    New work on Dollar

--- deploy_notifications.sh
+++ deploy_notifications.sh
@@ -16,37 +16,37 @@ then
     env_type="Live"
 fi
 
-${BUILD_UTILS_DIR}/raygun/deployment.sh -v "${RELEASE}" -t "$RAYGUN_EXTERNAL_TOKEN" -a "$RAYGUN_API_KEY"  -e "neil@vizz.buzz" -g "${CI_SHA1}" "Branch: ${CI_BRANCH} Project ${CI_PROJECT_REPONAME} Changes: $CI_COMPARE_URL Log: ${GITLOG}"
+#${BUILD_UTILS_DIR}/raygun/deployment.sh -v "${RELEASE}" -t "$RAYGUN_EXTERNAL_TOKEN" -a "$RAYGUN_API_KEY"  -e "neil@vizz.buzz" -g "${CI_SHA1}" "Branch: ${CI_BRANCH} Project ${CI_PROJECT_REPONAME} Changes: $CI_COMPARE_URL Log: ${GITLOG}"
 
 DATE_TODAY=$(date +"%Y-%m-%d")
 
 curl --data "key=${RESCUE_TIME_KEY}&highlight_date=$DATE_TODAY&description=${CI_PROJECT_REPONAME}+${CI_BRANCH}+${RELEASE}&source=${env_type}+Deployment" https://www.rescuetime.com/anapi/highlights_post
 #
-curl  -X POST -H "Content-type: application/json" -d "{\"title\":\"${CI_PROJECT_REPONAME} deployed ${RELEASE} for ${CI_BRANCH} \",\"text\":\"${CI_PROJECT_REPONAME} deployed ${RELEASE} for ${CI_BRANCH}, please see $CI_COMPARE_URL\",\"tags\":[\"${CI_PROJECT_REPONAME}\",\"${CI_BRANCH}\",\"${RELEASE}\"],\"alert_type\":\"info\"}" "https://app.datadoghq.com/api/v1/events?api_key=${DATADOG_API_KEY}"
+#curl  -X POST -H "Content-type: application/json" -d "{\"title\":\"${CI_PROJECT_REPONAME} deployed ${RELEASE} for ${CI_BRANCH} \",\"text\":\"${CI_PROJECT_REPONAME} deployed ${RELEASE} for ${CI_BRANCH}, please see $CI_COMPARE_URL\",\"tags\":[\"${CI_PROJECT_REPONAME}\",\"${CI_BRANCH}\",\"${RELEASE}\"],\"alert_type\":\"info\"}" "https://app.datadoghq.com/api/v1/events?api_key=${DATADOG_API_KEY}"
 
-curl -d "apiKey=30d3924e3694ac68c5743ba00e4bf0aa&appVersion=${RELEASE}" https://notify.bugsnag.com/deploy
+#curl -d "apiKey=30d3924e3694ac68c5743ba00e4bf0aa&appVersion=${RELEASE}" https://notify.bugsnag.com/deploy
 
-curl https://sentry.io/api/hooks/release/builtin/106345/8a24068e454450b1689a71197893e46720dc007836a718af0edd29a7b2194d9f/ \
-  -X POST \
-  -H 'Content-Type: application/json' \
-  -d "{\"version\": \"${RELEASE}\"}"
+#curl https://sentry.io/api/hooks/release/builtin/106345/8a24068e454450b1689a71197893e46720dc007836a718af0edd29a7b2194d9f/ \
+#  -X POST \
+#  -H 'Content-Type: application/json' \
+#  -d "{\"version\": \"${RELEASE}\"}"
 
-if  [[ ${CI_BRANCH} == "master" ]]
-then
-read -d '' payload <<EOF
-{
-  "icon_emoji": ":tada:",
-  "text": "Version ${RELEASE} codenamed ${CODENAME} released into live",
-  "attachments": [
-    {
-      "title": "New Release",
-      "title_link": "https://bizz.vizz.buzz/version/${RELEASE_NUMBER}",
-      "text": "https://bizz.vizz.buzz/version/${RELEASE_NUMBER}",
-      "color": "#4E86A8"
-    }
-  ]
-}
-EOF
-curl -X POST --data-urlencode "payload=$payload" https://chat.vizz.buzz/hooks/3fWh2rxoeuSWWudPq/y5T73Gvvebnee3rprzh2LXhT4QZ8PNecfnAH25ya6FTu7FM9
-fi
+#if  [[ ${CI_BRANCH} == "master" ]]
+#then
+#read -d '' payload <<EOF
+#{
+#  "icon_emoji": ":tada:",
+#  "text": "Version ${RELEASE} codenamed ${CODENAME} released into live",
+#  "attachments": [
+#    {
+#      "title": "New Release",
+#      "title_link": "https://bizz.vizz.buzz/version/${RELEASE_NUMBER}",
+#      "text": "https://bizz.vizz.buzz/version/${RELEASE_NUMBER}",
+#      "color": "#4E86A8"
+#    }
+#  ]
+#}
+#EOF
+#curl -X POST --data-urlencode "payload=$payload" https://chat.vizz.buzz/hooks/3fWh2rxoeuSWWudPq/y5T73Gvvebnee3rprzh2LXhT4QZ8PNecfnAH25ya6FTu7FM9
+#fi
 
