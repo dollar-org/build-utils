0b2438a0c0a8113ab026ebf6d5defe0119a4dddc
neilellis <neil@vizz.buzz>
2017-07-10 00:06:24 +0100

    New work on Dollar

--- promote_from_staging.sh
+++ promote_from_staging.sh
@@ -123,5 +123,6 @@ echo ${RELEASE} ${RELEASE_NUMBER} ${RELEASE_ID} ${CODENAME}  > .release.details
 git add .release.details
 git commit -m "Release ${RELEASE}  [${RELEASE_NUMBER:-}/${RELEASE_ID:-}] (${CODENAME})" || :
 git tag ${TAG} || :
-git push --tags
 git push --set-upstream origin master
+git push --tags
+#git push origin master
