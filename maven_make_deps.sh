86571996669d1d1b7cf214a6c937c750a611eb6e
neilellis <neil@vizz.buzz>
2017-07-10 01:43:14 +0100

    New work on Dollar

--- maven_make_deps.sh
+++ maven_make_deps.sh
@@ -4,6 +4,9 @@ DIR=$(pwd)
 cd -
 mvn -v
 mkdir -p ~/.m2/
+echo "login=neilellis" > ~/.github
+echo "password=${GITHUB_PASSWORD}" >> ~/.github
+
 cp $DIR/settings.xml ~/.m2/settings.xml
 [[ ${CIRCLE_BRANCH} != 'master' ]] || ( mvn versions:set -DnewVersion=$(cat .release) && mvn versions:resolve-ranges && mvn versions:lock-snapshots )
 mvn install -e -q -Drat.skip -Dsource.skip=true -DgenerateReports=false -Dmaven.javadoc.skip=true -Dmaven.test.skip
