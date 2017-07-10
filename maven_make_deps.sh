c8f3f6bc6a8384dde075d098da3d8860232e150a
neilellis <neil@vizz.buzz>
2017-07-10 01:28:14 +0100

    New work on Dollar

--- maven_make_deps.sh
+++ maven_make_deps.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
+DIR=$(pwd)
+cd -
 mvn -v
 mkdir -p ~/.m2/
-cp settings.xml ~/.m2/settings.xml
+cp $DIR/settings.xml ~/.m2/settings.xml
 [[ ${CIRCLE_BRANCH} != 'master' ]] || ( mvn versions:set -DnewVersion=$(cat .release) && mvn versions:resolve-ranges && mvn versions:lock-snapshots )
 mvn install -e -q -Drat.skip -Dsource.skip=true -DgenerateReports=false -Dmaven.javadoc.skip=true -Dmaven.test.skip
