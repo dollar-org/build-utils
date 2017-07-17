f61ea4a1ac7d177b70228583580f1a1c2b8f8b9f
neilellis <neil@vizz.buzz>
2017-07-17 15:25:36 +0100

    New work on Dollar

--- maven_deploy.sh
+++ maven_deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
+DIR=$(pwd)
+cd -
+$DIR/set-version.sh
+
 git config --global user.email "hello@neilellis.me"
 git config --global user.name "Neil Ellis"
-mvn versions:set -DnewVersion=$(cat .release)
-mvn versions:resolve-ranges
-mvn versions:lock-snapshots
+
 mvn -q -T 1C -Dmaven.test.skip=true -Drat.skip=true -DskipEnforcer -Dmaven.javadoc.skip=true -DgenerateReports=false package
 mvn -q -T 2C -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser -Dmaven.test.skip=true -Drat.skip=true -DskipEnforcer -Dmaven.javadoc.skip=true -DgenerateReports=false deploy
