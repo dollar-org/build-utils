0edab4e26313d260c024a20a95d281038575b5f4
neilellis <neil@vizz.buzz>
2017-07-21 22:17:52 +0100

    New work on Dollar

--- maven_make_deps.sh
+++ maven_make_deps.sh
@@ -7,5 +7,5 @@ mkdir -p ~/.m2/
 echo "login=neilellis" > ~/.github
 echo "password=${GITHUB_PASSWORD}" >> ~/.github
 cp $DIR/settings.xml ~/.m2/settings.xml
-$DIR/set-version.sh
+#$DIR/set-version.sh
 mvn install -e -q -Drat.skip -Dsource.skip=true -DgenerateReports=false -Dmaven.javadoc.skip=true -Dmaven.test.skip
