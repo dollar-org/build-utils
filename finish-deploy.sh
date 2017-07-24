99d3f1ea8163e5401e8059e65a2f2ef7ef15246e
neilellis <neil@vizz.buzz>
2017-07-24 15:50:33 +0100

    New work on Dollar

--- finish-deploy.sh
+++ finish-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp README.staged.md README.md
+git add README.md
 git config --global user.email "hello@neilellis.me"
 git config --global user.name "Neil Ellis"
-git commit -m "Release completion step for $(cat .release) [skip ci]" || :
+git commit -am "Release completion step for $(cat .release) [skip ci]" || :
 git push origin master
