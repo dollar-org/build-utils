afc4ad37ff69776328f46a1bb3e9388be8d95b52
neilellis <neil@vizz.buzz>
2017-07-26 16:09:50 +0100

    New work on Dollar

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 . functions.sh
 CODENAME_1=$(codenames/name.sh 1234567890123456 arcane_jobs)
 CODENAME_2=$(codenames/name.sh 1234567890123456 names)
 CODENAME_3=$(codenames/name.sh 1234567890123456 shortwords)
 [[ $CODENAME_1 == "thoughtful-carnifex-from-trent-vale" ]]
 [[ $CODENAME_2 == "tobye-niccolo-nessie" ]]
-[[ $CODENAME_3 == "pproud-glazed-barons " ]]
+[[ $CODENAME_3 == "proud-glazed-barons" ]]
+echo "PASSED"
