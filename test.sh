6efce658123707d3ac94561daa1ff49d007889bd
neilellis <neil@vizz.buzz>
2017-07-26 15:55:17 +0100

    New work on Dollar

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ set -eux
 CODENAME_1=$(codenames/name.sh 1234567890123456 arcane_jobs)
 CODENAME_2=$(codenames/name.sh 1234567890123456 names)
 CODENAME_3=$(codenames/name.sh 1234567890123456 shortwords)
-[[ $CODENAME_1 == "thoughtful-carnifex-from-trent_and_mersey_canal" ]]
+[[ $CODENAME_1 == "thoughtful-carnifex-from-trent-vale" ]]
 [[ $CODENAME_2 == "tobye-niccolo-nessie" ]]
 [[ $CODENAME_3 == "pproud-glazed-barons " ]]
