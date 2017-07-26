df95d416554e2972501afc51797dcff279bf1064
neilellis <neil@vizz.buzz>
2017-07-26 21:27:34 +0100

    New work on Dollar

--- finish-deploy.sh
+++ finish-deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eux
+git pull
 cp README.staged.md README.md
 git add README.md
 git config --global user.email "hello@neilellis.me"
 git config --global user.name "Neil Ellis"
-git pull
 github_changelog_generator  --token=${GITHUB_CHANGELOG_TOKEN} --release-branch master --future-release ${NEXT_MAJOR_VERSION}
 git add CHANGELOG.md
 git commit -am "Release completion step for $(cat .release) [skip ci]" || :
