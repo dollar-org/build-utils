0e002e9e70e4a7a4d81154d4ed9c083829a2078a
neilellis <neil@vizz.buzz>
2017-07-26 17:41:25 +0100

    New work on Dollar

--- set-version.sh
+++ set-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
-if [[ ${CIRCLE_BRANCH:-dev} != 'master' ]] ; then
+if [[ ${CIRCLE_BRANCH:-dev} == 'master' ]] ; then
  mvn -e -q versions:set -DnewVersion=$(cat .release)
  mvn -q versions:resolve-ranges
  mvn -q versions:lock-snapshots
-fi 
+fi
