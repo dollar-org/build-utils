15c570a11426f969137fefa27abb07a15eb4355c
neilellis <neil@vizz.buzz>
2017-08-21 18:06:33 +0100

    New work on Dollar

--- finish-deploy.sh
+++ finish-deploy.sh
@@ -7,5 +7,5 @@ git config --global user.email "hello@neilellis.me"
 git config --global user.name "Neil Ellis"
 github_changelog_generator  --token=${GITHUB_CHANGELOG_TOKEN} --release-branch master --future-release ${NEXT_MAJOR_VERSION}
 git add CHANGELOG.md
-git commit -am "Release completion step for $(cat .release) [skip ci]" || :
+git commit -am "✔ Release completion step for $(cat .release) [skip ci]" || :
 git push origin master
